7a17c4dd3adbfad6dab615ed8cf14f58cec57895
Vikas Dahiya <vikas@Vikas-Dahiyas-MacBook-Pro-2.local>
2019-03-26 22:13:49 -0700

    adding test method to test the jenkins build

--- src/test/java/com/sclabs/useraccount/service/UserCreationServiceTest.java
+++ src/test/java/com/sclabs/useraccount/service/UserCreationServiceTest.java
@@ -3,6 +3,7 @@ package com.sclabs.useraccount.service;
 import com.sclabs.useraccount.repository.UserInfo;
 import com.sclabs.useraccount.repository.UserInfoRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserCreationServiceTest {
@@ -18,6 +19,11 @@ public class UserCreationServiceTest {
 
     @Before
     public void setValuefor_UserInfo_InBeforeTest(){
+
+    }
+
+    @Test
+    public void dummyTest(){
 
     }
 }
